7ab22e044a0d8b713c21ae38c87c21988f2cf332
Arduino-05 <larduinobatista@gmail.com>
2024-03-26 12:49:05 -0300

    Complemento

---
+++ contas.py
@@ -0,0 +1,7 @@
+contas = [
+    {"id" : 1, "nome" : "luiz", "total" : 15}
+    {"id" : 2, "nome" : "arthur", "total" : 15}]
+
+depesas = [
+    {"id" : 1,"data": "08/05", "descriçoes":(''), "conta_id":1,}
+]
---
+++ numeros.py
@@ -0,0 +1,9 @@
+numero1 = float(input("Digite o primeiro numero: "))
+
+
+numero2 = float(input("Digite a segunda nota: "))
+    
+if numero1 > numero2 :
+    print(f"O numero {numero1} e maior que {numero2}")
+elif numero2 > numero1 :
+    print(f"O numero {numero2} e maior que {numero1}")
